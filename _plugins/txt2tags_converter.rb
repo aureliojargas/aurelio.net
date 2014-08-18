6d7a7e7b5b4c4ad4bb941db05f44fb8bf6128ca3
Aurelio Jargas <verde@aurelio.net>
2014-08-18 20:24:01 -0300

    Use a specific txt2tags version from SVN
    This is the version where the html5 target is OK for my usage.

--- _plugins/txt2tags_converter.rb
+++ _plugins/txt2tags_converter.rb
@@ -23,7 +23,7 @@ module Jekyll
 
 			tmpfile = '/tmp/jekyll.t2t'
 			File.open(tmpfile, 'w') { |f| f.write("\n" + content) }
-			`txt2tags -t html5 --no-headers --css-sugar -i #{tmpfile} -o -`
+			`txt2tags-2.6.1102 -t html5 --no-headers --css-sugar -i #{tmpfile} -o -`
 		end
 	end
 end
