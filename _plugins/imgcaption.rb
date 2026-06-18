11f99da93ef2a79c7309b24196ea57687df43fc4
Aurelio Jargas <verde@aurelio.net>
2026-06-18 18:44:38 +0200

    Bump Jekyll from 3.10.0 to 4.4.1, adopt Kramdown
    Since Jekyll version 3.0 (2015) the default Markdown parser has changed
    from redcarpet to Kramdown. I postponed adoting Kramdown as long as I
    could, because of the differences in features and details.
    
    The full explanation is in issue 71.
    
    My plugin for the `<figure>` tag needed an update to account for the
    extra space before autoclosing tags with `/>`. Redcarpet did not add
    that space.

--- _plugins/imgcaption.rb
+++ _plugins/imgcaption.rb
@@ -7,26 +7,26 @@
 # Images with no caption (alt="" attribute) are just marked with an 'image' class.
 #
 #   Markdown: ![](http://example.com/image.jpg)
-#   HTML:     <p><img src="http://example.com/image.jpg" alt=""/></p>
-#   Plugin:   <figure><img src="http://example.com/image.jpg" alt=""/></figure>
+#   HTML:     <p><img src="http://example.com/image.jpg" alt="" /></p>
+#   Plugin:   <figure><img src="http://example.com/image.jpg" alt="" /></figure>
 #
 # The same happens for images with no caption, but surrounded by a link.
 #
 #   Markdown: [![](http://example.com/image.jpg)](http://example.com)
-#   HTML:     <p><a href="http://example.com"><img src="http://example.com/image.jpg" alt=""/></a></p>
-#   Plugin:   <figure><a href="http://example.com"><img src="http://example.com/image.jpg" alt=""/></a></figure>
+#   HTML:     <p><a href="http://example.com"><img src="http://example.com/image.jpg" alt="" /></a></p>
+#   Plugin:   <figure><a href="http://example.com"><img src="http://example.com/image.jpg" alt="" /></a></figure>
 #
 # Images with caption are marked with the <figure> tag.
 #
 #   Markdown: ![caption1](http://example.com/image.jpg)
-#   HTML:     <p><img src="http://example.com/image.jpg" alt="caption1"/></p>
-#   Plugin:   <figure><img src="http://example.com/image.jpg" alt="caption1"/><figcaption>caption1</figcaption></figure>
+#   HTML:     <p><img src="http://example.com/image.jpg" alt="caption1" /></p>
+#   Plugin:   <figure><img src="http://example.com/image.jpg" alt="caption1" /><figcaption>caption1</figcaption></figure>
 #
 # Also works for images with caption and surrounded by a link.
 #
 #   Markdown: [![caption2](http://example.com/image.jpg)](http://example.com)
-#   HTML:     <p><a href="http://example.com"><img src="http://example.com/image.jpg" alt="caption2"/></a></p>
-#   Plugin:   <figure><a href="http://example.com"><img src="http://example.com/image.jpg" alt="caption2"/></a><figcaption>caption2</figcaption></figure>
+#   HTML:     <p><a href="http://example.com"><img src="http://example.com/image.jpg" alt="caption2" /></a></p>
+#   Plugin:   <figure><a href="http://example.com"><img src="http://example.com/image.jpg" alt="caption2" /></a><figcaption>caption2</figcaption></figure>
 
 module Jekyll
 	module ImgcaptionFilter
@@ -37,16 +37,16 @@ module Jekyll
 			return input unless current_page && current_page['path'] && current_page['path'].end_with?('.md')
 
 			input.gsub(
-				/<p>(<img src=".+?" alt=""\/>)<\/p>/,
+				/<p>(<img src=".+?" alt="" \/>)<\/p>/,
 				'<figure>\1</figure>'
 			).gsub(
-				/<p>(<a href=".*?"><img src=".+?" alt=""\/><\/a>)<\/p>/,
+				/<p>(<a href=".*?"><img src=".+?" alt="" \/><\/a>)<\/p>/,
 				'<figure>\1</figure>'
 			).gsub(
-				/<p>(<img src=".+?" alt="(.+)"\/>)<\/p>/,
+				/<p>(<img src=".+?" alt="(.+)" \/>)<\/p>/,
 				'<figure>\1<figcaption>\2</figcaption></figure>'
 			).gsub(
-				/<p>(<a href=".*?"><img src=".+?" alt="(.+)"\/><\/a>)<\/p>/,
+				/<p>(<a href=".*?"><img src=".+?" alt="(.+)" \/><\/a>)<\/p>/,
 				'<figure>\1<figcaption>\2</figcaption></figure>'
 			)
 		end
